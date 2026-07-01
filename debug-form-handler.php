389378418b992017fcdc9ed7440e0d3631d7eb10
yazyanderson <119087620+yazyanderson@users.noreply.github.com>
2026-07-01 16:36:01 -0400

    for loop

--- debug-form-handler.php
+++ debug-form-handler.php
@@ -1,13 +1,23 @@
 <?php
 echo '<h1>Form input values</h1>';
+
 echo '<p>Your Name: ' . htmlspecialchars($_POST['visitor_name'] ?? '') . '</p>';
 
 $options = $_POST['options'] ?? [];
+
 if (!is_array($options)) {
     $options = [$options];
 }
 
-echo '<p>Options: ' . htmlspecialchars(implode(', ', $options)) . '</p>';
+echo '<p>Options:</p>';
+
+if (count($options) > 0 && $options[0] !== '') {
+    for ($i = 0; $i < count($options); $i++) {
+        echo htmlspecialchars($options[$i]) . '<br>';
+    }
+} else {
+    echo 'No options selected';
+}
 
 echo '<h2>All Form Data</h2>';
 echo '<pre>';
